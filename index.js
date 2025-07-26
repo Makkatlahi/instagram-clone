cde9a1597eddce5d0c482e95dc40d6e058deba52
Sean Mafnas <seanvincent671@gmail.com>
2025-07-26 20:29:39 +1000

    Update CSS for improved layout and responsiveness; enhance JavaScript to dynamically render posts with like functionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // const likeButtonEl = document.querySelector(".post__respond--like");
 const likeButtonEl = document.getElementById("heartIcon");
 const numLikesEl = document.querySelector("#numLikes");
+const articlesEl = document.querySelector(".articles");
 
 const posts = [
   {
@@ -33,12 +34,72 @@ const posts = [
   },
 ];
 
-function handleLike() {
-  likeButtonEl.classList.add(".hidden");
-  let currentLikes = parseInt(numLikesEl.textContent);
-  numLikesEl.textContent = currentLikes + 1;
+function renderPost() {
+  articlesEl.innerHTML = "";
+  let articles = "";
+  for (let i = 0; i < posts.length; i++) {
+    articles += `
+        <article class="container">
+          <section class="profile">
+            <img
+              class="profile__avatar"
+              src="${posts[i].avatar}"
+              alt="${posts[i].name}"
+            />
+            <div class="profile__info">
+              <h2 class="profile__info--name">${posts[i].name}</h2>
+              <p class="profile__info--location">${posts[i].location}</p>
+            </div>
+          </section>
+          <section class="post">
+            <img
+              class="post__img"
+              src="${posts[i].post}"
+              alt="${posts[i].name}"
+            />
+            <div class="post__respond">
+              <button class="post__respond--like">
+                <img
+                  class="post__respond--like-img"
+                  id="heartIcon"
+                  src="./images/icon-heart.png"
+                  alt="heart outline."
+                />
+                <img
+                  class="hidden"
+                  id="heartIconLiked"
+                  src="./images/heartIcon.png"
+                  alt="heart outline."
+                />
+              </button>
+              <button class="post__respond--comment">
+                <img
+                  id="commentIcon"
+                  src="./images/icon-comment.png"
+                  alt="Comment icon."
+                />
+              </button>
+              <button class="post__respond--dm">
+                <img
+                  id="messageIcon"
+                  src="./images/icon-dm.png"
+                  alt="Direct Message"
+                />
+              </button>
+            </div>
+            <div class="post__feedback">
+              <p class="post__feedback--likes embolden">
+                <span id="numLikes">${posts[i].likes}</span> likes
+              </p>
+              <p class="post__feedback--comments">
+                <span class="embolden">${posts[i].username}</span> ${posts[i].comment}
+              </p>
+            </div>
+          </section>
+        </article>
+`;
+  }
+  articlesEl.innerHTML = articles;
 }
 
-likeButtonEl.addEventListener("click", handleLike);
-
-function renderPost() {}
+renderPost();
